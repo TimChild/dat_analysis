aaa7c57a33bb66d449c952d557b04e803b321c27
Tim Child <timjchild@phas.ubc.ca>
2020-02-02 16:16:48 -0800

    temp files

--- src/temp.py
+++ src/temp.py
@@ -8,23 +8,18 @@ import src.DFcode.DFutil as DU
 
 
 
-
-class testclass(object):
-    def __init__(self):
-        self.a = 10
-        self.b = 20
-        self.c = 50
-
-    def add(self):
-        print(f'a + b = {self.a+self.b}')
-
-    def multiply(self):
-        print(f'a*b = {self.a*self.b}')
-
-    def addc(self):
-        print(f'a+c = {self.a+self.c}')
-
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import src.PlottingFunctions as PF
+import src.Scratch.temp as temp
+import inspect
 
 if __name__ == '__main__':
-    with open('testpkl.pkl', 'rb') as f:
-        two = pickle.load(f)
+
+    fig, ax = plt.subplots(1)
+
+    # PF.add_figtext(fig, 'How is this working? whatabout with a really long text that goes way too far for the figure how does it work then??? ')
+    # PF.add_fig_info(fig)
+    stack = temp.call_add_fig_info(fig)
+    for f in stack:
+        print(f'Index = {f.index}, Filename = {f.filename[-40:]}, Function = {f.function}, code_context = {f.code_context}, frame = {f.frame}, lineno = {f.lineno}')
