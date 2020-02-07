870eb8cdedd1267f82b2652133668d4860174788
Tim Child <timjchild@phas.ubc.ca>
2020-02-06 21:27:08 -0800

    Added StandardImports which should cover most normal plotting scripts

---
+++ src/Scripts/StandardImports.py
@@ -0,0 +1,13 @@
+"""Has most imports for normal plotting scripts"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import src.CoreUtil as CU
+import src.DFcode.DatDF as DF
+import src.DFcode.SetupDF as SF
+import src.PlottingFunctions as PF
+import src.Configs.Main_Config as cfg
+from src.Experiment import make_dat_standard
