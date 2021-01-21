3ae757b5ab42502de549e28931656d15e6a143c9
Tim Child <timjchild@phas.ubc.ca>
2021-01-20 19:28:20 -0800

    Added plot Avg

--- Analysis/Jan2021/Signal2Entropy.py
+++ Analysis/Jan2021/Signal2Entropy.py
@@ -25,23 +25,49 @@ class SquareEntropyPlotter:
     def plot_raw(self) -> go.Figure:
         z = self.dat.Data.i_sense
         z = z[0]
-        # z = np.mean(z, axis=0)
 
-        fig = self.one_plotter.plot(z, mode='lines', title='Row 0 of I_sense')
+        fig = self.one_plotter.plot(z, mode='lines', title=f'Dat{self.dat.datnum}: Row 0 of I_sense')
         return fig
 
     def plot_cycled(self) -> go.Figure:
         z = self.dat.SquareEntropy.default_Output.cycled
+        x = self.dat.SquareEntropy.x
         z = z[0]
-        # z = np.mean(z, axis=0)
 
-        fig = self.one_plotter.plot(z, mode='lines', title='Row 0 of cycled')
+        fig = self.one_plotter.figure(title=f'Dat{self.dat.datnum}: Row 0 of cycled')
+        for row, label in zip(z, ['v0_0', 'vP', 'v0_1', 'vM']):
+            fig.add_trace(self.one_plotter.trace(row, name=label, x=x, mode='lines'))
 
         return fig
 
     def plot_avg(self) -> go.Figure:
         z = self.dat.SquareEntropy.default_Output.averaged
-        return self.one_plotter.plot(z, mode='lines')
+        x = self.dat.SquareEntropy.x
+        fig = self.one_plotter.figure(title=f'Dat{self.dat.datnum}: Centered and Averaged I_sense')
+
+        for row, label in zip(z, ['v0_0', 'vP', 'v0_1', 'vM']):
+            fig.add_trace(self.one_plotter.trace(row, name=label, x=x, mode='lines'))
+        return fig
+
+    def plot_entropy_signal(self) -> go.Figure:
+        z = self.dat.SquareEntropy.avg_entropy_signal
+        x = self.dat.SquareEntropy.x
+
+
+        fit_info = self.dat.Entropy.avg_fit
+        fit_x = self.dat.Entropy.avg_x
+        fit = fit_info.eval_fit(fit_x)
+
+        fig = self.one_plotter.figure(title=f'Dat{self.dat.datnum}: Average Entropy Signal')
+
+        fig.add_trace(self.one_plotter.trace(data=z, x=x, mode='lines', name='Entropy Signal'))
+        fig.add_trace(self.one_plotter.trace(data=fit, x=fit_x, mode='lines', name='Fit'))
+
+        self.one_plotter.add_textbox(fig, text=f'Fit Values:\n'
+                                               f'dS={fit_info.best_values.dS:.3f}',
+                                     position='TR')
+
+        return fig
 
 
 if __name__ == '__main__':
@@ -59,7 +85,9 @@ if __name__ == '__main__':
 
     plotters = [SquareEntropyPlotter(dat) for dat in dats]
     for plotter in plotters[0:1]:
-        plotter.plot_raw().show(renderer='browser')
-        plotter.plot_cycled().show(renderer='browser')
-        plotter.plot_avg().show(renderer='browser')
+        # plotter.plot_raw().show(renderer='browser')
+        # plotter.plot_cycled().show(renderer='browser')
+        # plotter.plot_avg().show(renderer='browser')
+        plotter.plot_entropy_signal().show(renderer='browser')
+
 
