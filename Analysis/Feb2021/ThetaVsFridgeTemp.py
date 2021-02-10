c4566633ed8c61dc838e9a08852632ff356ed0ca
Tim Child <timjchild@phas.ubc.ca>
2021-02-10 11:21:50 -0800

    Theta vs fridge temp (just temporary in feb21 analsysis)

---
+++ Analysis/Feb2021/ThetaVsFridgeTemp.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+from src.DatObject.Make_Dat import get_dat, get_dats
+import src.UsefulFunctions as U
+from src.DataStandardize.ExpSpecific.Feb21 import Feb21Exp2HDF, Feb21ExpConfig
+from src.DataStandardize.ExpConfig import ExpConfigGroupDatAttribute, ExpConfigBase
+import multiprocessing as mp
+import plotly.graph_objs as go
+import numpy as np
+import lmfit as lm
+from typing import TYPE_CHECKING, Iterable, Optional
+
+if TYPE_CHECKING:
+    from src.DatObject.Make_Dat import DatHDF
+    from src.DatObject.Attributes.DatAttribute import FitInfo
+
+
+
+def _fix_temps(dats):
+    for dat in dats:
+        dat.del_hdf_item('Experiment Config')
+        exp_config = ExpConfigGroupDatAttribute(dat, exp_config=Feb21ExpConfig(dat.datnum))
+        dat.ExpConfig = exp_config
+    return True
+
+
+def calc_transition(datnum: int) -> FitInfo:
+    dat = get_dat(datnum, exp2hdf=Feb21Exp2HDF)
+    tfit = dat.Transition.avg_fit
+    return tfit
+
+
+def theta_vs_fridge_temp_fig(thetas: Iterable[float], temps: Iterable[float], datnums: Iterable[int],
+                             lower_fit_temp_limit: Optional[float] = None) -> go.Figure:
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(mode='markers+lines+text', x=temps, y=thetas, text=datnums, name='Avg Thetas'))
+
+    if lower_fit_temp_limit is not None:
+        line = lm.models.LinearModel()
+        fit_thetas, fit_temps = np.array([(theta, temp) for theta, temp in zip(thetas, temps) if temp > lower_fit_temp_limit]).T
+        fit = line.fit(fit_thetas, x=fit_temps, nan_policy='omit')
+        x_range = np.linspace(0, max(temps), int(max(temps))+1)
+        fig.add_trace(go.Scatter(mode='lines', x=x_range, y=fit.eval(x=x_range),
+                                 name=f'Fit to temps > {lower_fit_temp_limit}'))
+
+    fig.update_layout(xaxis_title='Temp /mK', yaxis_title='Theta /mV',
+                      title=f'Dats{min(datnums)}-{max(datnums)}: Transition vs Fridge temp')
+
+    return fig
+
+
+if __name__ == '__main__':
+    # pool = mp.Pool(processes=6)
+    # datnums = range(613, 624+1)
+    # fits = pool.map(calc_transition, datnums)
+    # print(fits)
+
+    dats = get_dats((613, 624+1), exp2hdf=Feb21Exp2HDF)
+    thetas_ = [dat.Transition.avg_fit.best_values.theta for dat in dats]
+    temps_ = [300, 275, 250, 225, 200, 175, 150, 125, 100, 75, 50, 10]
+    datnums_ = [dat.datnum for dat in dats]
+    fig = theta_vs_fridge_temp_fig(thetas=thetas_, temps=temps_, datnums=datnums_, lower_fit_temp_limit=160)
+    fig.show(renderer='browser')
+
+
+
+
+
+
+
+
