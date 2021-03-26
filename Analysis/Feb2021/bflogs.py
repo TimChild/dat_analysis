e83da18ac284c21a0e1b6d8e70274bbad4e636f8
Tim Child <timjchild@phas.ubc.ca>
2021-03-26 11:17:16 -0700

    Quick plot of compressor temps

--- Analysis/Feb2021/bflogs.py
+++ Analysis/Feb2021/bflogs.py
@@ -4,9 +4,12 @@ import plotly.graph_objects as go
 
 from src.Dash.DatPlotting import OneD
 
+import os
+print(os.path.abspath('.'))
 
 
-df = pd.read_csv('../../Status_21-03-25.csv')
+# df = pd.read_csv('../../Status_21-03-25.csv')
+df = pd.read_csv('Status_21-03-26.csv')
 # df = pd.read_csv('Status_21-03-01.csv')
 
 
@@ -20,9 +23,11 @@ wo = arr[:, 51]
 x = arr[:, 1]
 
 
+
 plotter = OneD()
 plotter.MAX_POINTS = 1000000000
-fig = plotter.figure(xlabel='Time', ylabel='Temperature /C', title='PT Compressor Temperatures 25th Mar 21')
+# fig = plotter.figure(xlabel='Time', ylabel='Temperature /C', title='PT Compressor Temperatures 25th Mar 21')
+fig = plotter.figure(xlabel='Time', ylabel='Temperature /C', title='PT Compressor Temperatures 26th Mar 21')
 # fig = plotter.figure(xlabel='Time', ylabel='Temperature /C', title='PT Compressor Temperatures 1st Mar 21')
 for d, label in zip([oil, win, wo], ['Oil', 'Water In', 'Water Out']):
     fig.add_trace(plotter.trace(data=d, x=x, name=label, mode='lines'))
