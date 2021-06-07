a235f4ac6a890a47b8c2035d1e971b967c393775
Tim Child <timjchild@phas.ubc.ca>
2021-06-07 10:42:12 -0700

    Class made for Nrg2D plots

--- FinalFigures/Gamma/figure4.py
+++ FinalFigures/Gamma/figure4.py
@@ -5,7 +5,8 @@ from scipy.interpolate import interp1d, interp2d
 from scipy.signal import savgol_filter
 import plotly.express as px
 import plotly.graph_objects as go
-from typing import Tuple
+from typing import Tuple, Dict, Callable, Optional
+from dataclasses import dataclass
 
 from FinalFigures.Gamma.plots import dndt_2d
 from src.Dash.DatPlotting import OneD, TwoD
@@ -16,40 +17,98 @@ p1d = OneD(dat=None)
 p2d = TwoD(dat=None)
 
 
-def data_nrg_vs_gate_2d(which: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Generate the 2D data for NRG dN/dT vs Sweepgate plot"""
+@dataclass
+class Data2D:
+    x: np.ndarray
+    y: np.ndarray
+    data: np.ndarray
+
+
+@dataclass
+class Data1D:
+    x: np.ndarray
+    data: np.ndarray
+
+
+class Nrg2D:
     temperature = 50  # in sweepgate mV so ~100mK
-    sweep_x = np.linspace(-100, 100, 201)
-    gs = np.linspace(temperature/10, temperature*30, 100)
-    nrg_dndt_func = NRG_func_generator(which)
+    gs = np.linspace(temperature / 10, temperature * 30, 100)
+    g_over_ts = gs / temperature
+    ylabel = 'G/T'
 
-    g_over_ts = gs/temperature
+    def __init__(self, which_data: str, which_x: str):
+        self.which_data = which_data
+        self.which_x = which_x
 
-    data = np.array([nrg_dndt_func(sweep_x, 0, g, temperature)
-                     for g in gs])
+    def _get_title(self):
+        if self.which_x == 'sweepgate':
+            x_part = 'Sweep Gate'
+        elif self.which_x == 'occupation':
+            x_part = 'Occupation'
+        else:
+            raise NotImplementedError
+        if self.which_data == 'dndt':
+            data_part = 'dN/dT'
+        elif self.which_data == 'occupation':
+            data_part = 'Occupation'
+        else:
+            raise NotImplementedError
+        return f'NRG {data_part} vs {x_part}'
 
-    return sweep_x, g_over_ts, data
+    def _get_x(self):
+        if self.which_x == 'sweepgate':
+            return np.linspace(-100, 100, 201)
+        elif self.which_x == 'occupation':
+            raise NotImplementedError
+
+    def _get_x_label(self) -> str:
+        if self.which_x == 'sweepgate':
+            return "Sweep Gate (mV)"
+        elif self.which_x == 'occupation':
+            raise NotImplementedError
+
+    def _data(self) -> Data2D:
+        """Generate the 2D data for NRG"""
+        nrg_dndt_func = NRG_func_generator(self.which_data)
+        x = self._get_x()
+        data = np.array([nrg_dndt_func(x, 0, g, self.temperature)
+                         for g in self.gs])
+        data2d = Data2D(x=x, y=self.g_over_ts, data=data)
+        return data2d
+
+    def plot(self, data2d: Optional[Data2D] = None) -> go.Figure:
+        if data2d is None:
+            data2d = self._data()
+        fig = p2d.plot(x=data2d.x, y=data2d.y, data=data2d.data,
+                       xlabel=self._get_x_label(), ylabel=self.ylabel, title=self._get_title())
+        return fig
+
+    def run(self) -> go.Figure:
+        data2d = self._data()
+        fig = self.plot(data2d)
+        # TOOD: Save data here
+        # U.save_to_igor_itx()
+        return fig
 
 
-def plot_nrg_dndt_vs_gate_2d(x: np.ndarray, y: np.ndarray, dndt: np.ndarray) -> go.Figure:
-    fig = p2d.plot(x=x, y=y, data=dndt, xlabel='Sweep Gate (mV)', ylabel='G/T', title='NRG dN/dT vs Sweep Gate')
-    return fig
-
-
+class NrgOccVsGate2D:
 
 
 if __name__ == '__main__':
     from src.DatObject.Make_Dat import get_dats, get_dat
 
-    # NRG dN/dT vs sweep gate
-    xs, gts, dndt = data_nrg_vs_gate_2d(which='dndt')
-    fig = plot_nrg_dndt_vs_gate_2d(xs, gts, dndt)
-    fig.show()
 
-    # NRG Occupation vs sweep gate
-    xs, gts, dndt = data_nrg_vs_gate_2d(which='occupation')
-    fig = plot_nrg_dndt_vs_gate_2d(xs, gts, dndt)
-    fig.show()
+    # NRG dN/dT vs sweep gate (fixed T varying G)
+    Nrg2D(which_data='dndt', which_x='sweepgate').run().show()
+
+    # NRG Occupation vs sweep gate (fixed T varying G)
+    Nrg2D(which_data='occupation', which_x='sweepgate').run().show()
+
+
+
+
+
+    ##################
 
     nrg = NRGData.from_mat()
     nrg_dndt = nrg.dndt
