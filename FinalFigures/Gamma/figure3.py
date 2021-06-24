7c39a5651d5b9bd7e867d88decf1f44d31ac204b
Tim Child <timjchild@phas.ubc.ca>
2021-06-24 14:49:55 -0700

    Added Gamma ~1 to Fig 3 plot

--- FinalFigures/Gamma/figure3.py
+++ FinalFigures/Gamma/figure3.py
@@ -8,6 +8,9 @@ from FinalFigures.Gamma.plots import integrated_entropy, entropy_vs_coupling, ga
 from src.useful_functions import save_to_igor_itx, order_list, fig_to_data_json, data_to_json
 from src.plotting.Mpl.PlotUtil import set_default_rcParams
 
+
+
+
 if __name__ == '__main__':
     from src.dat_object.make_dat import get_dats, get_dat
 
@@ -41,7 +44,7 @@ if __name__ == '__main__':
     ##########################################################################
     # Data for integrated_entropy
     fit_name = 'forced_theta_linear'
-    all_dats = get_dats(range(2164, 2170 + 1, 3))  # + [get_dat(2213)]
+    all_dats = get_dats(range(2164, 2170 + 1, 3)) + [get_dat(2121)]  # + [get_dat(2213)]
 
     # all_dats = get_dats(chain(range(7322, 7361 + 1, 2), range(7378, 7399+1, 2), range(7400, 7421+1, 2)))
     # all_dats = order_list(all_dats, [dat.Logs.fds['ESC'] for dat in all_dats])
@@ -63,14 +66,14 @@ if __name__ == '__main__':
     save_to_igor_itx(file_path=f'fig3_integrated_entropy.itx',
                      xs=xs + [np.arange(len(gts))],
                      datas=int_entropies + [np.array(gts)],
-                     names=[f'int_entropy_{k}' for k in ['weak', 'med', 'strong']] + ['gts_for_int_vary_g'],
+                     names=[f'int_entropy_{k}' for k in ['weak', 'med', 'strong', 'similar']] + ['gts_for_int_vary_g'],
                      x_labels=['Sweep Gate (mV)'] * len(int_entropies) + ['index'],
                      y_labels=['Entropy (kB)'] * len(int_entropies) + ['G/T'])
 
-    data_to_json(datas=int_entropies + xs,
-                 names=[f'int_entropy_{i}' for i, _ in enumerate(int_entropies)] +
-                       [f'int_entropy_{i}_x' for i, _ in enumerate(xs)],
-                 filepath=r'D:\GitHub\dat_analysis\dat_analysis\Analysis\Feb2021\figs/data/3IntegratedEntropies.json'),
+    # data_to_json(datas=int_entropies + xs,
+    #              names=[f'int_entropy_{i}' for i, _ in enumerate(int_entropies)] +
+    #                    [f'int_entropy_{i}_x' for i, _ in enumerate(xs)],
+    #              filepath=r'D:\GitHub\dat_analysis\dat_analysis\Analysis\Feb2021\figs/data/3IntegratedEntropies.json'),
 
     # Plot Integrated Entropy
     fig, ax = plt.subplots(1, 1)
