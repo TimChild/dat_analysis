c1cddbe617caeb63a9aca5abaf55616d40065238
Tim Child <timjchild@phas.ubc.ca>
2021-11-22 12:15:14 -0800

    Fixed setup.py to point to the correct directory now

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='dat_analysis',
     version='2.0',
-    # packages=['dat_analysis', 'dat_analysis.plotting',
-    #           'dat_analysis.plotting.mpl', 'dat_analysis.plotting.plotly', 'dat_analysis.plotting.plotly.common_plots', 'dat_analysis.dat_object',
-    #           'dat_analysis.dat_object.attributes', 'dat_analysis.analysis_tools', 'dat_analysis.data_standardize',
-    #           'dat_analysis.data_standardize.exp_specific', 'tests'],
     packages=find_packages('dat_analysis'),
-    package_dir={'': 'dat_analysis'},
+    package_dir={'': 'src'},
     url='https://github.com/TimChild/dat_analysis',
     license='MIT',
     author='Tim Child',
