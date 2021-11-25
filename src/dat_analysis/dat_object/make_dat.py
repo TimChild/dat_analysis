73d86f8108d65e5a223393c4deaaf313301134c7
Tim Child <timjchild@phas.ubc.ca>
2021-11-24 20:11:30 -0800

    Fixed lock in DatHandler, added get_latest_datnum

--- src/dat_analysis/dat_object/make_dat.py
+++ src/dat_analysis/dat_object/make_dat.py
@@ -2,6 +2,7 @@
 make DatHDFs"""
 from __future__ import annotations
 import os
+import re
 import logging
 from singleton_decorator import singleton
 from typing import TYPE_CHECKING, Union, Iterable, Tuple, List, Optional
@@ -37,6 +38,17 @@ logger = logging.getLogger(__name__)
 # sync_lock = threading.Lock()
 
 
+def get_newest_datnum(last_datnum=None, exp2hdf=Nov21Exp2HDF):
+    """Get the newest datnum that already exists in Experiment data directory
+    (last_datnum is useful to pass if the location of dats changes after a certain datnum for example)
+    """
+    last_datnum = last_datnum if last_datnum else 0
+    exp2hdf = exp2hdf(last_datnum)
+    data_directory = exp2hdf.SysConfig.Directories.ddir
+    files = os.listdir(data_directory)
+    datnums = [re.findall(r'\d+', f.split('.')[0]) for f in files]  # Split the part before the extension
+    datnums = [int(num[0]) for num in datnums if num]  # Convert first found number to string if there were numbers found
+    return max(datnums)
 
 
 @singleton
@@ -48,23 +60,27 @@ class DatHandler(object):
     """
     open_dats = {}
     lock = threading.Lock()
+    sync_lock = threading.Lock()
 
     def get_dat(self, datnum: int, datname='base', overwrite=False,
                 init_level='min',
                 exp2hdf: Optional[Union[str, type(Exp2HDF)]] = None) -> DatHDF:
-        with self.lock:
-            if isinstance(exp2hdf, str):
-                if exp2hdf.lower() not in CONFIGS:
-                    raise KeyError(f'{exp2hdf} not found in {CONFIGS.keys()}')
-                exp2hdf = CONFIGS[exp2hdf.lower()](datnum=datnum, datname=datname)
-            elif hasattr(exp2hdf, 'ExpConfig'):  # Just trying to check it is an Exp2HDF without importing
-                exp2hdf = exp2hdf(datnum=datnum, datname=datname)
-            elif exp2hdf is None:
-                exp2hdf = default_Exp2HDF(datnum=datnum, datname=datname)
-            else:
-                raise RuntimeError(f"Don't know how to interpret {exp2hdf}")
+        if isinstance(exp2hdf, str):
+            if exp2hdf.lower() not in CONFIGS:
+                raise KeyError(f'{exp2hdf} not found in {CONFIGS.keys()}')
+            exp2hdf = CONFIGS[exp2hdf.lower()](datnum=datnum, datname=datname)
+        elif hasattr(exp2hdf, 'ExpConfig'):  # Just trying to check it is an Exp2HDF without importing
+            exp2hdf = exp2hdf(datnum=datnum, datname=datname)
+        elif exp2hdf is None:
+            exp2hdf = default_Exp2HDF(datnum=datnum, datname=datname)
+        else:
+            raise RuntimeError(f"Don't know how to interpret {exp2hdf}")
 
-            full_id = self._full_id(exp2hdf.ExpConfig.dir_name, datnum, datname)  # For temp local storage
+        full_id = self._full_id(exp2hdf.ExpConfig.dir_name, datnum, datname)  # For temp local storage
+        if not overwrite and full_id in self.open_dats:
+            return self.open_dats[full_id]
+
+        with self.lock:  # Only 1 thread should be making choices about deleting or creating new HDFs
             path = exp2hdf.get_datHDF_path()
             self._ensure_dir(path)
             if overwrite:
@@ -120,7 +136,7 @@ class DatHandler(object):
         if os.path.isfile(exp_path):
             return True
         else:
-            with self.lock:
+            with self.sync_lock:
                 if not os.path.isfile(exp_path):  # Might be there by time lock is released
                     exp2hdf.synchronize_data()  # Tries to synchronize data from server then check for path again.
                 if os.path.isfile(exp_path):
@@ -149,3 +165,7 @@ class DatHandler(object):
 
 get_dat = DatHandler().get_dat
 get_dats = DatHandler().get_dats
+
+
+if __name__ == '__main__':
+    print(get_newest_datnum())
