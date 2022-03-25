9cc739c1ec63d57db054694d3ea8e0efcbb4a8a2
Tim Child <timjchild@phas.ubc.ca>
2022-03-24 17:58:29 -0700

    Moved some stuff to dash_data_viewer, fleshed out first test
    Untested first test of using Process etc

--- src/dat_analysis/analysis_tools/new_procedures.py
+++ src/dat_analysis/analysis_tools/new_procedures.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import abc
-from typing import Any, List
+from typing import Any, List, Union, Optional, Dict, TYPE_CHECKING
 from functools import lru_cache
 from dataclasses import dataclass
 import plotly.graph_objects as go
@@ -11,10 +11,13 @@ import numpy as np
 import dash_bootstrap_components as dbc
 from dash import html, dcc
 
-
 from dat_analysis.plotting.plotly import OneD, TwoD
-from dat_analysis.useful_functions import get_matching_x
+from dat_analysis.useful_functions import get_matching_x, get_data_index
 from dat_analysis.hdf_util import set_attr, get_attr, DatDataclassTemplate
+from dat_analysis.dat_object.attributes.square_entropy import get_transition_part
+
+if TYPE_CHECKING:
+    from dash.development.base_component import Component
 
 
 @dataclass
@@ -40,14 +43,15 @@ class PlottableData:
             self.y = np.arange(self.data.shape[-2])
         if self.z is None and ndim >= 3:
             self.z = np.arange(self.data.shape[-3])
-        if self.axes is None and ndim >= 4:
+        if self.axes is None:
             self.axes = np.array([np.arange(s) for s in self.data.shape])
 
         # Make sure axes have correct dimensions for plotting data
         for ax, dim in zip([self.x, self.y, self.z], [-1, -2, -3]):
             if ax is not None:
-                assert ax.shape[-1] == self.data.shape[dim]
-        assert np.all([a.shape for a in self.axes] == self.data.shape)
+                if ax.shape[-1] != self.data.shape[dim]:
+                    print(ax.shape, self.data.shape, dim)
+        # assert np.all([a.shape for a in self.axes] == self.data.shape)
 
 
 @dataclass
@@ -76,7 +80,7 @@ class DataPlotter:
         data = self.data.data
         if axis is None:
             axis = self.data.x
-        if slice is not None:
+        if s_ is not None:
             data = data[s_]
 
         axis = get_matching_x(axis, data)
@@ -127,26 +131,24 @@ class Process(abc.ABC):
 
     E.g. splitting data into square wave parts
 
-    mixture of human friendly input, as well as data passed in, no loading from file
+    mixture of human friendly input, as well as data passed in, no loading input data from file (only save or restore
+    whole process to or from and open HDF Group)
     """
     default_name = 'SubProcess'  # Name of this Process (e.g. group name in HDF)
 
     def __init__(self):
         self.child_processes: List[Process] = []  # if calling other processes, append to this list (e.g. in input_data)
+        self._data_input: Dict[str, Union[np.ndarray, Any]] = {}  # Store data as provided
+        self._data_preprocessed: Dict[
+            str, Union[np.ndarray, Any]] = {}  # Store data from sub processes or basic pre-processing
+        self._data_output: Dict[str, Union[np.ndarray, Any]] = {}  # Store data produced
 
     @abc.abstractmethod
-    def input_data(self, data) -> Any:
+    def input_data(self, *args, **kwargs):
         """
-        Pass minimal data in to get job done and call any sub processes here adding them to children
-        e.g.
-        self.inputs = dict(...)
-        sub_process = OtherProcess(...)
-        self.child_processes.append(sub_process)
-        part_processed = sub_process.output_data(...)
+        Pass minimal data in to get job done
 
-        self.data_to_process = PlottableData(..., part_processed.x, ...)
-
-        return self.data_to_process
+        self._data_input = {'data': ..., 'y': ...}
 
         Returns:
 
@@ -154,19 +156,33 @@ class Process(abc.ABC):
         pass
 
     @abc.abstractmethod
+    def preprocess(self):
+        """
+        Do basic preprocessing or call any sub processes here adding them to children
+
+        Note: This need not be saved to HDF, should be quick to run again
+        (child_process should be quick to load if previously run)
+
+        sub_process = OtherProcess(...)
+        self.child_processes.append(sub_process)
+
+        sub_process.input_data(...)
+        part_processed = sub_process.output_data(...)
+
+        self._data_preprocessed = {..., 'data': part_processed.data, ...}
+
+        e.g.
+        Returns:
+        """
+
+    @abc.abstractmethod
     def output_data(self) -> PlottableData:  # Note: can make more different outputs as well
         """
-        Do something with self.data_to_process
-        Anything intensive should be put in a cached self._process or similar
+        Do something with self._data_input and self._data_preprocessed
 
         Returns:
 
         """
-        raise NotImplementedError
-
-    @lru_cache
-    def _process(self):
-        """Do the actual processing in cached function"""
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -174,7 +190,7 @@ class Process(abc.ABC):
         """
         Initialize the DataPlotter with reasonable title and labels etc
         i.e.
-        return DataPlotter(self.data_to_process(...), self.inputs, ...)
+        return DataPlotter(self._data_preprocessed.x, self._data_input.data, ...)
         Returns:
 
         """
@@ -195,6 +211,9 @@ class Process(abc.ABC):
     def save_progress(self, group: h5py.Group, **kwargs):
         """
         Save necessary info to given hdf group/file with minimal additional input (in a single group)
+
+        i.e. save inputs and outputs. Preprocessed data should not need to be saved.
+        (If processing is quick, and data large, can also not save outputs and just recalculate them in load_progress)
         """
         self._save_progress_default(group)
         # Any data to save for this Process specifically (i.e. inputs/outputs)
@@ -274,131 +293,148 @@ class Process(abc.ABC):
                 sub_processes.append(sub_process)
         return sub_processes
 
-
-class ProcessViewer(abc.ABC):
-    """
-    Take a process and combine input/output views into a nice overall summary... Something which can be easily plotted
-    in a dash page. or output to a plotly multigraph fig or mpl, or saved to pdf
-    """
-    def __init__(self, process: Process, **options):
-        self.process = process
-
-    @abc.abstractmethod
-    def dash_full(self, **more_options):  # -> Component
-        """
-        Combine multiple figures/text/etc to make an output component
-        (can assume 3/4 page wide, any height)
-        Args:
-            **more_options ():
-
-        Returns:
-
-        """
-        input_plotter = self.process.get_input_plotter()
-        output_plotter = self.process.get_output_plotter()
-
-        input_fig = ProcessComponentOutputGraph(fig=input_plotter.plot_1d())
-        output_fig = ProcessComponentOutputGraph(fig=output_plotter.plot_1d())
-
-        return html.Div([
-            input_fig.layout(),
-            output_fig.layout()
-        ])
-
-        pass
-
-    @abc.abstractmethod
-    def mpl_full(self, **more_options):  # -> plt.Figure
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def pdf_full(self, **more_options):  # -> pdf
-        raise NotImplementedError
-
-
-class ProcessInterface(abc.ABC):
-    """
-    Things necessary to put a process into a dash page or report with easy human friendly input. I.e. for building a
-    new dash page
-
-    human friendly input, and id of file to load from (or data passed in)
-    """
-    def __init__(self):
-        self.store_id = ''
-        self.sub_store_ids = []
-
-    @abc.abstractmethod
-    def required_input_components(self) -> List[ProcessComponentInput]:
-        """
-        Give the list of components that need to be placed in order for Process to be carried out
-        Should all update stores or possibly even the dat file
-        Returns:
-
-        """
-        return []
-
-    @abc.abstractmethod
-    def all_outputs(self) -> List[ProcessComponentOutput]:
-        """Lit of components that display the process in detail"""
-        return []
-
-    @abc.abstractmethod
-    def main_outputs(self) -> List[ProcessComponentOutput]:
-        """List of components that display the main features of the process"""
-        return []
-
-
-class ProcessComponent(abc.ABC):
-    """
-    A dash component with callbacks etc to interface with user
-    """
-    @abc.abstractmethod
-    def run_callbacks(self, **kwargs):
-        pass
-
-    @abc.abstractmethod
-    def layout(self):
-        pass
-
-
-class ProcessComponentInput(ProcessComponent, abc.ABC):
-    """
-    Component mostly for user input
-    """
-    def __init__(self, title, num_buttons, etc):
-        super().__init__()
-
-
-class ProcessComponentOutput(ProcessComponent, abc.ABC):
-    """
-    Component mostly for output to graph/table/file etc
-    """
-
-
-class ProcessComponentOutputGraph(ProcessComponentOutput):
-    """
-    Component for displaying a figure (with additional options)
-    """
-    def __init__(self, fig: go.Figure):
-        super().__init__()
-        self.fig = fig
-
-    def layout(self):
-        return dcc.Graph(figure=self.fig)
-
-    def run_callbacks(self, **kwargs):
-        pass
-
-
 #####################################################################################################
 
 
+# Now to create this process for separating square wave i_sense data into separate parts
+class SeparateSquareProcess(Process):
+    def input_data(self, i_sense_2d: np.ndarray, x: np.ndarray,
+                   measure_frequency: float,
+                   samples_per_setpoint: int,
+                   setpoint_average_delay: float,
+
+                   y: Optional[np.ndarray] = None,
+                   ):
+        self._data_input = {
+            'i_sense': i_sense_2d,
+            'x': x,
+            'measure_freq': measure_frequency,
+            'samples_per_setpoint': samples_per_setpoint,
+            'setpoint_average_delay': setpoint_average_delay,
+            'y': y,
+        }
+
+    def preprocess(self):
+        i_sense = np.atleast_2d(self._data_input['i_sense'])
+        y = self._data_input['y']
+        y = y if y is not None else np.arange(i_sense.shape[-2]),
+
+        data_by_setpoint = i_sense.reshape((i_sense.shape[0], -1, 4, self._data_input['samples_per_setpoint']))
+
+        delay_index = round(self._data_input['setpoint_average_delay'] * self._data_input['measure_freq'])
+        assert delay_index < self._data_input['samples_per_setpoint']
+
+        setpoint_duration = self._data_input['samples_per_setpoint'] / self._data_input['measure_freq']
+
+        self._data_preprocessed = {
+            'y': y,
+            'data_by_setpoint': data_by_setpoint,
+            'delay_index': delay_index,
+            'setpoint_duration': setpoint_duration,
+        }
+
+    def output_data(self,
+                    ) -> dict:
+        separated = np.mean(
+            self._data_preprocessed['data_by_setpoint'][:, :, :, self._data_preprocessed['delay_index']:], axis=-1)
+
+        x = self._data_input['x']
+        x = np.linspace(x[0], x[-1], separated.shape[-1])
+        y = self._data_preprocessed['y']
+        self._data_output = {
+            'x': x,
+            'separated': separated,
+            'y': y,
+        }
+        return self._data_output
+
+    def get_input_plotter(self,
+                          xlabel: str = 'Sweepgate /mV', data_label: str = 'Current /nA',
+                          title: str = 'Data Averaged to Single Square Wave',
+                          start_x: Optional[float] = None, end_x: Optional[float] = None,  # To only average between
+                          start_y: Optional[float] = None, end_y: Optional[float] = None,  # To only average between
+                          ) -> DataPlotter:
+        by_setpoint = self._data_preprocessed['data_by_setpoint']
+        x = self._data_input['x']
+        y = self._data_preprocessed['y']
+
+        if start_y or end_y:
+            indexes = get_data_index(y, [start_y, end_y])
+            s_ = np.s_[indexes[0], indexes[1]]
+            by_setpoint = by_setpoint[s_]  # slice of rows, all_dac steps, 4 parts, all datapoints
+
+        if start_x or end_x:
+            indexes = get_data_index(x, [start_x, end_x])
+            s_ = np.s_[indexes[0], indexes[1]]
+            by_setpoint = by_setpoint[:, s_]  # All rows, slice of dac steps, 4 parts, all datapoints
+
+        averaged = np.nanmean(by_setpoint, axis=0)  # Average rows together
+        averaged = np.moveaxis(averaged, 1, 0)  # 4 parts, num steps, samples
+        averaged = np.nanmean(averaged, axis=-1)  # 4 parts, num steps
+        averaged = averaged.flatten()  # single 1D array with all 4 setpoints sequential
+
+        duration = self._data_preprocessed['setpoint_duration']
+        time_x = np.linspace(0, 4 * duration, averaged.shape[-1])
+
+        data = PlottableData(
+            data=averaged,
+            x=time_x,
+        )
+
+        plotter = DataPlotter(
+            data=data,
+            xlabel=xlabel,
+            data_label=data_label,
+            title=title,
+        )
+        return plotter
+
+    def get_output_plotter(self,
+                           xlabel: str = 'Sweepgate /mV', data_label: str = 'Current* /nA',
+                           ylabel: str = 'Repeats',
+                           part: Union[str, int] = 'cold',  # e.g. hot, cold, vp, vm, or 0, 1, 2, 3
+                           title: str = 'Separated into Square Wave Parts',
+                           xspacing: float = 0,
+                           yspacing: float = 0.3,
+                           ) -> DataPlotter:
+        separated = self._data_output['separated']  # rows, 4 parts, dac steps
+        separated = np.moveaxis(separated, 2, 1)
+        print(separated.shape)
+
+        data_part = get_transition_part(separated, part)  # TODO: I think separated shape is wrong order
+
+        data = PlottableData(
+            data=data_part,
+            x=self._data_output['x'],
+            y=self._data_output['y'],
+        )
+        plotter = DataPlotter(
+            data=data,
+            xlabel=xlabel,
+            ylabel=ylabel,
+            data_label=data_label,
+            title=title,
+            xspacing=xspacing,
+            yspacing=yspacing,
+        )
+        return plotter
+
+    def save_progress(self, group: h5py.Group, **kwargs):
+        self._write_to_group(group, 'data_input', self._data_input)
+        if self._data_output:
+            set_attr(group, 'data_output', self._data_output)
+
+    @classmethod
+    def load_progress(cls, group: h5py.Group) -> Process:
+        inst = cls()
+        inst._data_input = cls._read_from_group(group, 'data_input')
+        inst.preprocess()
+        inst._data_output = cls._read_from_group(group, 'data_output', default=dict(), check_exists=False)
+        return inst
 
 
 if __name__ == '__main__':
     import dash
-    from dash import html
-    import dash_bootstrap_components as dbc
 
     app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
