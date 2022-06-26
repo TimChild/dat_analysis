0197af48c24b5d7a0a238de96a27f8bbc48a13ff
Tim Child <timjchild@phas.ubc.ca>
2022-06-25 18:46:03 -0700

    Use environment variable to find config.toml

--- src/dat_analysis/new_dat/new_dat_util.py
+++ src/dat_analysis/new_dat/new_dat_util.py
@@ -4,11 +4,20 @@ General utility functions related to the new simpler dat HDF interface
 import os
 import toml
 from typing import Optional
+import logging
+logger = logging.getLogger(__name__)
 
-from ..core_util import get_project_root
 
-root = get_project_root()
-config_path = os.path.join(root, 'config.toml')
+config_path = os.environ.get('DatAnalysisConfig', None)
+if config_path is None:
+    msg = f'No "DatAnalysisConfig" environment variable found, there will be no default config. \n\n'\
+          f'For first setup, you need to provide a path to a "config.toml" file (note: may have to change\n '\
+          f'permissions depending on how you python is installed). If the file empty or even non-existent (\n'\
+          f'as long as a file can be created there), a default template will be made the next time you run \n'\
+          f'the program. \n' \
+          f'You may have to restart your python environment (e.g. jupyter, pycharm, etc).\n\n'
+    logger.warning(msg)
+    print(msg)
 
 
 def default_config():
@@ -28,7 +37,7 @@ def get_local_config(path: Optional[str] = None) -> dict:
     if not path:
         path = config_path
 
-    if not os.path.exists(path):
+    if path and not os.path.exists(path):
         with open(path, 'w') as f:
             toml.dump(default_config(), f)
 
--- tests/new_dat/test_new_dat_util.py
+++ tests/new_dat/test_new_dat_util.py
@@ -1,11 +1,20 @@
 from unittest import TestCase
 import toml
+import os
 from dat_analysis.new_dat.new_dat_util import get_local_config, default_config
 
 test_config_path = 'test_config.toml'
 
 
 class Test(TestCase):
+    def test_get_environ_config(self):
+        p = 'test_config.toml'
+        os.environ['TestDatAnalysisConfig'] = p
+        config_path = os.environ.get('TestDatAnalysisConfig', None)
+        self.assertEqual(p, config_path)
+
+
+
     def test_get_local_config(self):
         config = get_local_config(path=test_config_path)
         self.assertIsInstance(config, dict)
