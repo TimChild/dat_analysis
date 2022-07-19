b4771ce620cc14711575eac09c147aedc499066e
Tim Child <timjchild@phas.ubc.ca>
2022-07-19 09:17:44 -0700

    simpler dat will be v3.0.0. Added more description

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('long_description.txt', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='dat_analysis',
-    version='2.0',
+    version='3.0.0',
     packages=find_packages('dat_analysis'),
     package_dir={'': 'src'},
     url='https://github.com/TimChild/dat_analysis',
@@ -10,6 +13,7 @@ setup(
     author='Tim Child',
     author_email='timjchild@gmail.com',
     description='Python Analysis Package for Folk Lab at UBC',
+    long_description=long_description,
     install_requires=[
         'pandas',
         'numpy',
