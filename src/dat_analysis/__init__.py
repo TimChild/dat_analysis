c4aee10ffa75054d4cf7f01ac33793f6529684fb
Tim Child <timjchild@phas.ubc.ca>
2022-08-09 15:32:05 -0700

    add get_dat to top level __init__ for ease of access

--- src/dat_analysis/__init__.py
+++ src/dat_analysis/__init__.py
@@ -1,4 +1,5 @@
 # from .dat_object.make_dat import get_dat, get_dats
 from .new_dat.new_dat_util import get_local_config
+from .new_dat.dat_hdf import get_dat, get_dat_from_exp_filepath
 
 config = get_local_config()
