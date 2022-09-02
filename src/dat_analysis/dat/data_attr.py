252ce8f2081478915f1d095918e52aa0612f67bd
Tim Child <timjchild@phas.ubc.ca>
2022-09-02 15:40:26 -0700

    Default to NotFoundInHdfError for missing data

--- src/dat_analysis/dat/data_attr.py
+++ src/dat_analysis/dat/data_attr.py
@@ -5,6 +5,7 @@ Still require a general way to interact with datasets in the HDF file
 import h5py
 
 from ..hdf_file_handler import HDFFileHandler
+from ..hdf_util import NotFoundInHdfError
 
 _NOT_SET = object()
 
@@ -13,6 +14,8 @@ class Data:
     def __init__(self, hdf_path: str, path_to_logs_group: str):
         self._hdf_path = hdf_path
         self._group_path = path_to_logs_group
+        self._x = None
+        self._y = None
 
     @property
     def hdf_read(self):
@@ -24,11 +27,15 @@ class Data:
 
     @property
     def x(self):
-        return self.get_data('x_array', None)
+        if not self._x:
+            self._x = self.get_data('x_array', None)
+        return self._x
 
     @property
     def y(self):
-        return self.get_data('y_array', None)
+        if not self._y:
+            self._y = self.get_data('y_array', None)
+        return self._y
 
     @property
     def data_keys(self):
@@ -43,7 +50,7 @@ class Data:
         elif default is not _NOT_SET:
             data = default
         else:
-            data = None
+            raise NotFoundInHdfError(f'{key} not found. Existing keys are {self.data_keys}')
         return data
 
     def _get_all_data_keys(self):
